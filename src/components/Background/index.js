2b70943a6d1ea81ba81db2159645e42c81021cb8
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2019-08-06 17:16:48 -0700

    added background component

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#286AA6', '#35A0CD'],
+})`
+  flex: 1;
+`;
