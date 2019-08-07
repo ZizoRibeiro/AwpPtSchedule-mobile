591cc1fa7896d76b5203f32bc4896e131eb503f5
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2019-08-06 18:32:20 -0700

    added input and button to the sign in page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
 import Background from '~/components/Background';
+import Button from '~/components/Button';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>Signing</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Enter your name"
+      />
+
+      <Button>Enter</Button>
     </Background>
   );
 }
